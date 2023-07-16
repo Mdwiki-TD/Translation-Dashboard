0944c4e5aaa46e1cd0e78165e619725ebe56f4af
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-07-17 02:36:03 +0300

    Update header.php

--- header.php
+++ header.php
@@ -42,15 +42,15 @@ function print_head() {
 		<link href='/Translation_Dashboard/css/dashboard_new1.css' rel='stylesheet' type='text/css'>
 		<link href='$hoste/ajax/libs/font-awesome/5.15.3/css/all.min.css' rel='stylesheet' type='text/css'>
 		<link href='$hoste/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' rel='stylesheet' type='text/css'>
-		<link href='$hoste/ajax/libs/datatables.net-bs5/1.13.1/dataTables.bootstrap5.css' rel='stylesheet' type='text/css'>
+		<link href='$hoste/ajax/libs/datatables.net-bs5/1.13.5/dataTables.bootstrap5.css' rel='stylesheet' type='text/css'>
 		<link href='$hoste/ajax/libs/jqueryui/1.13.2/themes/base/jquery-ui.min.css' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="$hoste/ajax/libs/bootstrap-select/1.14.0-beta3/css/bootstrap-select.css" rel='stylesheet' type='text/css'>
 
-		<script src='$hoste/ajax/libs/jquery/3.6.3/jquery.min.js'></script>
+		<script src='$hoste/ajax/libs/jquery/3.7.0/jquery.min.js'></script>
 		<script src='$hoste/ajax/libs/popper.js/2.11.8/umd/popper.min.js'></script>
 		<script src='$hoste/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js'></script>
 		<script src='$hoste/ajax/libs/datatables.net/2.1.1/jquery.dataTables.min.js'></script>
-		<script src='$hoste/ajax/libs/datatables.net-bs5/1.13.1/dataTables.bootstrap5.min.js'></script>
+		<script src='$hoste/ajax/libs/datatables.net-bs5/1.13.5/dataTables.bootstrap5.min.js'></script>
 		<script src='$hoste/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js'></script>
 		<script src="$hoste/ajax/libs/bootstrap-select/1.14.0-beta3/js/bootstrap-select.min.js"></script>
 
