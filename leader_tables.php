71b0a73b7865c8f5747270f37d5216036cd06bff
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2023-07-17 03:42:45 +0300

    Update leader_tables.php

--- leader_tables.php
+++ leader_tables.php
@@ -1,84 +1,86 @@
 <?PHP
-
 include_once('tables.php');
 include_once('functions.php');
 include_once('langcode.php');
-
 include_once('sql_tables.php'); // $sql_qids $cat_titles $cat_to_camp $camp_to_cat
 
-$year      = $_REQUEST['year'] ?? 'all';
-$camp      = $_REQUEST['camp'] ?? 'all';
-$project   = $_REQUEST['project'] ?? 'all';
+$year = $_REQUEST['year'] ?? 'all';
+$camp = $_REQUEST['camp'] ?? 'all';
+$project = $_REQUEST['project'] ?? 'all';
 
-if ($camp == 'all' && isset($_REQUEST['cat'])) $camp = $cat_to_camp[$_REQUEST['cat']] ?? 'all';
+if ($camp == 'all' && isset($_REQUEST['cat'])) {
+    $camp = $cat_to_camp[$_REQUEST['cat']] ?? 'all';
+}
 
-$camp_cat  = $camp_to_cat[$camp] ?? '';
+$camp_cat = $camp_to_cat[$camp] ?? '';
 
-$qua_all_part1_group = <<<SQL
-select
-p.target, p.cat, p.lang, p.word, year(p.pupdate) as pup_y, p.user, u.user_group
-from pages p, users u
-SQL;
-
-$qua_all_part1 = <<<SQL
-select
-p.target, p.cat, p.lang, p.word, year(p.pupdate) as pup_y, p.user, 
-(select u.user_group from users u WHERE p.user = u.username) as user_group
-from pages p
-SQL;
-
-$qua_all_part2 = "
-where p.target != ''
+$qua_all_part1_group = "
+    SELECT
+    p.target, p.cat, p.lang, p.word, YEAR(p.pupdate) AS pup_y, p.user, u.user_group
+    FROM pages p, users u
 ";
 
-if ($camp != 'all' && $camp_cat != '') $qua_all_part2 .= "and p.cat = '$camp_cat' \n";
-if ($year != 'all')         $qua_all_part2 .= "and year(p.pupdate) = '$year' \n";
+$qua_all_part1 = "
+    SELECT
+    p.target, p.cat, p.lang, p.word, YEAR(p.pupdate) AS pup_y, p.user, 
+    (SELECT u.user_group FROM users u WHERE p.user = u.username) AS user_group
+    FROM pages p
+";
+
+$qua_all_part2 = "
+    WHERE p.target != ''
+";
+
+if ($camp != 'all' && $camp_cat != '') {
+    $qua_all_part2 .= "AND p.cat = '$camp_cat' \n";
+}
+
+if ($year != 'all') {
+    $qua_all_part2 .= "AND YEAR(p.pupdate) = '$year' \n";
+}
+
 if ($project != 'all') {
     $qua_all_part1 = $qua_all_part1_group;
-    $qua_all_part2 .= "and p.user = u.username \n";
-    $qua_all_part2 .= "and u.user_group = '$project' \n";
-}; 
+    $qua_all_part2 .= "AND p.user = u.username \n";
+    $qua_all_part2 .= "AND u.user_group = '$project' \n";
+}
 
 $qua_all = $qua_all_part1 . $qua_all_part2;
 
-if (isset($_REQUEST['test'])) echo $qua_all;
+if (isset($_REQUEST['test'])) {
+    echo $qua_all;
+}
 
 $Words_total = 0;
 $Articles_numbers = 0;
 $global_views = 0;
-
 $sql_users_tab = array();
-
 $Users_word_table = array();
-
 $sql_Languages_tab = array();
-
 $all_views_by_lang = array();
-
 $Views_by_users = array();
 $Views_by_target = array();
 
-$qua_vi = <<<SQL
-select target, countall, count2021, count2022, count2023
-from views;
-SQL;
+$qua_vi = "
+SELECT target, countall, count2021, count2022, count2023
+FROM views;
+";
 
-foreach ( execute_query($qua_vi) AS $k => $tab ) {
+foreach (execute_query($qua_vi) as $k => $tab) {
     $Views_by_target[$tab['target']] = array(
         'all'  => $tab['countall'],
         '2021' => $tab['count2021'],
         '2022' => $tab['count2022'],
         '2023' => $tab['count2023']
     );
-};
+}
 
-foreach ( execute_query($qua_all) AS $Key => $teb ) {
-    
-    $cat    = $teb['cat'];
-    $lang   = $teb['lang'];
-    $user   = $teb['user'];
-    $tat    = $teb['target'];
-    $word   = $teb['word'];
+foreach (execute_query($qua_all) as $Key => $teb) {
+    $cat = $teb['cat'];
+    $lang = $teb['lang'];
+    $user = $teb['user'];
+    $tat = $teb['target'];
+    $word = $teb['word'];
     
     $coco = $Views_by_target[$tat][$year] ?? 0;
     
@@ -87,24 +89,27 @@ foreach ( execute_query($qua_all) AS $Key => $teb ) {
     $global_views += $coco;
     
     if (!isset($all_views_by_lang[$lang])) $all_views_by_lang[$lang] = 0;
+    
     $all_views_by_lang[$lang] += $coco;
     
-    if (!isset($sql_Languages_tab[$lang]))       $sql_Languages_tab[$lang] = 0;
-    $sql_Languages_tab[$lang] = $sql_Languages_tab[$lang] + 1 ;
+    if (!isset($sql_Languages_tab[$lang])) $sql_Languages_tab[$lang] = 0;
+    
+    $sql_Languages_tab[$lang] += 1;
     
     if (!isset($Users_word_table[$user])) $Users_word_table[$user] = 0;
-    $Users_word_table[$user] += $word ;
+
+    
+    $Users_word_table[$user] += $word;
     
     if (!isset($Views_by_users[$user])) $Views_by_users[$user] = 0;
+    
     $Views_by_users[$user] += $coco;
     
     if (!isset($sql_users_tab[$user])) $sql_users_tab[$user] = 0;
-    $sql_users_tab[$user] = $sql_users_tab[$user] + 1 ;
     
-    };
-
+    $sql_users_tab[$user] += 1;
+}
 function createNumbersTable($c_user, $c_articles, $c_words, $c_langs, $c_views) {
-    
     $Numbers_table = <<<HTML
     <table class="sortable table table-striped"> <!-- scrollbody -->
     <thead>
@@ -125,7 +130,6 @@ function createNumbersTable($c_user, $c_articles, $c_words, $c_langs, $c_views) 
     
     return $Numbers_table;
 };
-
 function makeUsersTable() {
     
     global $sql_users_tab, $Users_word_table, $Views_by_users;
@@ -177,7 +181,6 @@ function makeUsersTable() {
     
     return $text;
 }
-
 function makeLangTable() {
     
     global $lang_code_to_en, $code_to_lang, $sql_Languages_tab, $all_views_by_lang;
@@ -241,7 +244,3 @@ function makeLangTable() {
     
     return $text;
 }
-
-
-
-?>
