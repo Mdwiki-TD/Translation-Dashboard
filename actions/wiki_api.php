18fd40b10559a898a04732c4419b0951aa0d083a
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2024-03-08 05:33:54 +0300

    Update wiki_api.php

--- actions/wiki_api.php
+++ actions/wiki_api.php
@@ -5,6 +5,7 @@ function make_view_by_number($target, $numb, $lang, $pupdate) {
     $numb2 = ($numb != '') ? $numb : "?";
     $start = $pupdate != '' ? $pupdate : '2019-01-01';
     $end = date("Y-m-d", strtotime("yesterday"));
+
     $url = 'https://pageviews.wmcloud.org/?' . http_build_query( array(
         'project' => "$lang.wikipedia.org",
         'platform' => 'all-access',
@@ -16,8 +17,11 @@ function make_view_by_number($target, $numb, $lang, $pupdate) {
         'pages' => $target,
     ));
     $start2 = $pupdate != '' ? str_replace('-', '', $pupdate) : '20190101';
+
     $url2 = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/' . $lang . '.wikipedia/all-access/all-agents/' . rawurlencode($target) . '/daily/' . $start2 . '/2030010100';
+
     $link = "<a target='_blank' href='$url'>$numb2</a>";
+
     if ($numb2 == '?' || $numb2 == 0 || $numb2 == '0') {
         $link = "<a target='_blank' name='toget' hrefjson='$url2' href='$url'>$numb2</a>";
     };
@@ -30,10 +34,11 @@ function get_views($target, $lang, $pupdate) {
     $url = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/' . $lang . '.wikipedia/all-access/all-agents/' . rawurlencode($target) . '/daily/' . $start2 . '/2030010100';
     $output = file_get_contents( $url );
     $result = json_decode( $output, true );
-    $view = 0;
-    foreach ($result['items'] AS $da){
-        $view += $da['views'];
-    };
+
+    // $view = 0;
+    // foreach ($result['items'] AS $da) $view += $da['views'];
+
+    $view = isset($result['items']) ? array_sum(array_column($result['items'], 'views')) : 0;
+
     return $view;
 };
-?>
