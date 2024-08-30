e87cdaf63f3e82f9c1dce50987dfeba18853a48f
ibrahem <ibrahem.al-radaei@outlook.com>
2024-08-30 03:56:10 +0300

    .

---
+++ publish/fix_refs.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Publish\FixRefs;
+
+/*
+Usage:
+include_once __DIR__ . '/../publish/fix_refs.php';
+use function Publish\FixRefs\fix_wikirefs;
+*/
+
+function fix_wikirefs($wikitext, $lang)
+{
+    // ---
+
+    // ---
+    return $wikitext;
+}
