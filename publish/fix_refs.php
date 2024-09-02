6a2c1aa63d21d6638313cd699d508c7f1d2ab104
ibrahem <ibrahem.al-radaei@outlook.com>
2024-09-02 04:18:19 +0300

    Update fix_refs.php

--- publish/fix_refs.php
+++ publish/fix_refs.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Publish\FixRefs;
 
 /*
@@ -8,10 +7,31 @@ include_once __DIR__ . '/../publish/fix_refs.php';
 use function Publish\FixRefs\fix_wikirefs;
 */
 
+include_once __DIR__ . '/../../fixwikirefs/fix.php';
+
+function endsWith($string, $endString)
+{
+    $len = strlen($endString);
+    return substr($string, -$len) === $endString;
+};
+
 function fix_wikirefs($wikitext, $lang)
 {
     // ---
-
+    $resultb = get_text_results($wikitext, $lang);
     // ---
+    $resultb = $resultb['output'] ?? '';
+    // ---
+    $resultb = trim($resultb);
+    // ---
+    $t3 = endsWith($resultb, '.txt');
+    //---
+    if ($t3) {
+        $newtext = file_get_contents($resultb);
+        if ($newtext != '') {
+            return $newtext;
+        };
+    };
+    //---
     return $wikitext;
 }
