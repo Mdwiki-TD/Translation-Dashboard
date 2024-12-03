487654d2e989de98006f16c94efa2f991b2dd822
ibrahem <ibrahem.al-radaei@outlook.com>
2024-12-03 03:28:02 +0300

    Update darkmode.php

--- darkmode.php
+++ darkmode.php
@@ -22,34 +22,29 @@
 function dark_mode_icon()
 {
     return <<<HTML
-        <button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme"
-            type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static"
-            aria-label="Toggle theme (light)">
-            <svg class="bi my-1 theme-icon-active">
-                <use href="#sun-fill"></use>
-            </svg>
+        <button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown"
+            data-bs-display="static" aria-label="Toggle theme (light)">
+            <span class="me-2 opacity-50 theme-icon-active">
+                <i class="bi bi-sun-fill"></i>
+            </span>
             <span class="d-lg-none ms-2" id="bd-theme-text"></span>
         </button>
         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
             <li>
-                <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="light"
-                    aria-pressed="true">
+                <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="light" aria-pressed="true">
                     <i class="bi bi-sun-fill me-2 opacity-50 theme-icon"></i> Light
                 </button>
             </li>
             <li>
-                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark"
-                    aria-pressed="false">
+                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
                     <i class="bi bi-moon-stars-fill me-2 opacity-50 theme-icon"></i> Dark
                 </button>
             </li>
             <li>
-                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="auto"
-                    aria-pressed="false">
+                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="auto" aria-pressed="false">
                     <i class="bi bi-circle-half me-2 opacity-50 theme-icon"></i> Auto
                 </button>
             </li>
         </ul>
-
     HTML;
 }
