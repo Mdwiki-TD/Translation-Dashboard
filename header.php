f0a1a1e07968ab2c2bc28664d8cde3a62ab61b2c
ibrahem <ibrahem.al-radaei@outlook.com>
2024-12-06 02:58:26 +0300

    1

--- header.php
+++ header.php
@@ -36,17 +36,41 @@ echo "
 $hoste = '';
 //---
 $user_in_coord = false;
-$coord_tools = '<a href="tools.php" class="nav-link py-2 px-0 px-lg-2"><span class="navtitles"></span>Tools</a>';
+$coord_tools = '<a href="tdc/tools.php" class="nav-link py-2 px-0 px-lg-2"><span class="navtitles"></span>Tools</a>';
 //---
 if (in_array(global_username, $coordinators)) {
-	$coord_tools = '<a href="coordinator.php" class="nav-link py-2 px-0 px-lg-2"><span class="navtitles"></span>Coordinator Tools</a>';
+	$coord_tools = '<a href="tdc/index.php" class="nav-link py-2 px-0 px-lg-2"><span class="navtitles"></span>Coordinator Tools</a>';
 	$user_in_coord = true;
 };
 //---
 define('user_in_coord', $user_in_coord);
 //---
-require("darkmode.php");
-$them_li = dark_mode_icon();
+$them_li = <<<HTML
+	<button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown"
+		data-bs-display="static" aria-label="Toggle theme (light)">
+		<span class="theme-icon-active my-1">
+			<i class="bi bi-sun-fill"></i>
+		</span>
+		<span class="d-lg-none ms-2" id="bd-theme-text"></span>
+	</button>
+	<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
+		<li>
+			<button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="light" aria-pressed="true">
+				<i class="bi bi-sun-fill me-2 opacity-50 theme-icon"></i> Light
+			</button>
+		</li>
+		<li>
+			<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
+				<i class="bi bi-moon-stars-fill me-2 opacity-50 theme-icon"></i> Dark
+			</button>
+		</li>
+		<li>
+			<button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="auto" aria-pressed="false">
+				<i class="bi bi-circle-half me-2 opacity-50 theme-icon"></i> Auto
+			</button>
+		</li>
+	</ul>
+HTML;
 //---
 $li_user = <<<HTML
 	<li class="nav-item col-4 col-lg-auto">
