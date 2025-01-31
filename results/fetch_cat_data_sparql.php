9953b54bb7b0700cf1c235c1834160af536ba122
ibrahem <ibrahem.al-radaei@outlook.com>
2025-02-01 00:05:02 +0300

    1

---
+++ results/fetch_cat_data_sparql.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Results\FetchCatDataSparql;
+
+/*
+Usage:
+
+use function Results\FetchCatDataSparql\get_cat_exists_and_missing;
+
+*/
+
+use function Results\GetCats\get_mdwiki_cat_members;
+use function Results\GetCats\open_json_file;
+use function Actions\Functions\test_print;
+
+function print_r_it($data, $title)
+{
+    echo "$title:" . count($data) . "<br>";
+    echo "<pre>";
+    print(json_encode($data));
+    echo "</pre>";
+}
+function get_cat_exists_and_missing($cat, $camp, $depth, $code, $use_cache = true): array
+{
+    // Fetch category members
+    $members_to = get_mdwiki_cat_members($cat, $use_cache = $use_cache, $depth = $depth, $camp = $camp);
+
+    print_r_it($members_to, 'members_to');
+
+    $members = array_values($members_to); // Flatten the array
+
+    print_r_it($members, 'members');
+
+    test_print("Members size: " . count($members));
+
+    // Determine the directory for JSON files
+    $tables_dir = getenv('tables_dir') ?: __DIR__ . '/../../td/Tables';
+    // if (substr($tables_dir, 0, 2) === 'I:') {
+    if (str_starts_with($tables_dir, 'I:')) {
+        $tables_dir = 'I:/mdwiki/mdwiki/public_html/td/Tables';
+    }
+
+    // Load existing pages from JSON file
+    $json_file = "$tables_dir/cash_exists/$code.json";
+    $exists = open_json_file($json_file);
+    test_print("$json_file: Exists size: " . count($exists));
+
+    // Find missing members
+    $missing = array_diff($members, $exists);
+
+    $missing = array_unique($missing);
+
+    // Calculate the length of existing pages
+    $exs_len = count($members) - count($missing);
+
+    test_print("End of get_cat_exists_and_missing <br>===============================");
+
+    return [
+        "len_of_exists" => $exs_len,
+        "missing" => $missing,
+    ];
+}
--- results/results.php
+++ results/results.php
@@ -1,8 +1,11 @@
 <?PHP
+
 namespace Results\Results;
 //---
 include_once __DIR__ . '/tr_link.php';
 include_once __DIR__ . '/results_table.php';
+include_once __DIR__ . '/fetch_cat_data.php';
+include_once __DIR__ . '/fetch_cat_data_sparql.php';
 include_once __DIR__ . '/get_results.php';
 include_once __DIR__ . '/getcats.php';
 
