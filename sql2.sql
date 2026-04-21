d2bfe92136af63cce65c936853aa91316cd0c26a
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-21 04:36:34 +0300

    Update sql2.sql

--- sql2.sql
+++ sql2.sql
@@ -26,13 +26,6 @@ CREATE TABLE
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE
-    all_articles_titles (
-        qid varchar(120),
-        title varchar(255),
-        category varchar(255)
-    );
-
-CREATE TABLE
     all_exists (
         id int NOT NULL AUTO_INCREMENT,
         article_id varchar(255) NOT NULL,
@@ -63,13 +56,6 @@ CREATE TABLE
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE
-    all_qids_titles (
-        qid varchar(255),
-        title varchar(120),
-        category varchar(255)
-    );
-
-CREATE TABLE
     publish_reports (
         id int NOT NULL AUTO_INCREMENT,
         date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -85,29 +71,25 @@ CREATE TABLE
             CONSTRAINT publish_reports_chk_1 CHECK (json_valid (data))
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-CREATE VIEW all_articles_titles AS
+CREATE VIEW
+    all_articles_titles AS
 select
     q.qid AS qid,
     aa.article_id AS title,
     aa.category AS category
 from
-    (
-        all_articles aa
-        left join qids q on (aa.article_id = q.title)
-    );
+    all_articles aa
+    left join qids q on aa.article_id = q.title;
 
-CREATE VIEW all_qids_titles AS
+CREATE VIEW
+    all_qids_titles AS
 select
     qq.qid AS qid,
     q.title AS title,
     aa.category AS category
 from
-    (
-        (
-            all_qids qq
-            left join qids q on (qq.qid = q.qid)
-        )
-        left join all_articles aa on (aa.article_id = q.title)
-    );
+    all_qids qq
+    left join qids q on qq.qid = q.qid
+    left join all_articles aa on aa.article_id = q.title;
 
 -- 2026-04-21 01:21:15 UTC
