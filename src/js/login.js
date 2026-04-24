7a39ce17ee8c977215f3ae3447918dcce2ad20cf
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-24 04:40:02 +0300

    Update login.js

--- src/js/login.js
+++ src/js/login.js
@@ -1,14 +1,14 @@
 
 function login() {
-    var cat = $('#cat').val() || '';
+    var camp = $('#camp').val() || '';
     var code = $('#code').val() || '';
     var type = $('input[name=type]:checked').val() || '';
     var test = $('#test').val() || '';
     // var url = 'https://mdwiki.toolforge.org/auth/index.php?a=login';
     var url = '/auth/index.php?a=login';
     // var url = 'login/index.php?doit=1';
-    if (cat !== '') {
-        url += '&cat=' + encodeURIComponent(cat);
+    if (camp !== '') {
+        url += '&camp=' + encodeURIComponent(camp);
     }
     if (code !== '') {
         url += '&code=' + encodeURIComponent(code);
