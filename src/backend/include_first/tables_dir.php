543c83ef01bd474493d080589f27e18e810eb34d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-13 03:09:03 +0300

    .

--- src/backend/include_first/tables_dir.php
+++ src/backend/include_first/tables_dir.php
@@ -20,12 +20,12 @@ function open_td_tables_file($path, $echo = true)
 {
 	//---
 	// $home_dir = getenv("HOME") ?: 'I:/MD_TOOLS/MDWIKI_MAIN_REPO';
-	// $json_tables_path = $home_dir . '/public_html/td/Tables';
+	// $tables_path = $home_dir . '/public_html/td/Tables';
 	//---
 	$settings = Settings::getInstance();
-	$json_tables_path = $settings->TablesPath;
+	$tables_path = $settings->TablesPath;
 	//---
-	$file_path = "$json_tables_path/jsons/$path";
+	$file_path = "$tables_path/jsons/$path";
 	//---
 	if (!is_file($file_path)) {
 		test_print("---- open_td_tables_file: file $file_path does not exist");
