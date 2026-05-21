3cecb8c5dd589bc05ea32dab93b59c56035a4b9e
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-22 02:50:57 +0300

    Update fetch_cat_data.php

--- src/backend/results/new_way/fetch_cat_data.php
+++ src/backend/results/new_way/fetch_cat_data.php
@@ -14,36 +14,34 @@ use function TD\Render\TestPrint\test_print;
 
 function get_cat_exists_and_missing_new($exists_via_td, $cat, $depth, $code, $use_cache = true): array
 {
-    // ---
-    // Fetch category members
     $members = get_mdwiki_cat_members($cat, $depth, $use_cache);
-    // ---
+
     test_print("get_cat_exists and_missing Members size: " . count($members));
-    // ---
+
     // pages that exist in $exists and $members
-    // ---
+
     $exists = array_filter($exists_via_td, function ($item) use ($members) {
         return in_array($item['title'], $members);
     });
-    // ---
+
     $func = function ($item) {
+        $title = $item['title'] ?? '';
         return [
             "qid" => $item["qid"] ?? "",
-            "title" => $item["title"] ?? "",
+            "title" => $title,
             "target" => $item["target"] ?? "",
             "via" => "td",
             "pupdate" => $item["pupdate"] ?? "",
             "user" => $item["user"] ?? ""
         ];
     };
-    // ---
+
     $exists = array_map($func, $exists);
-    // ---
+
     // Find missing members
     $missing = array_diff($members, array_keys($exists));
-    // ---
     $missing = array_values(array_unique($missing));
-    // ---
+
     // test_print("End of get_cat exists_and_missing <br>===============================");
     return [$exists, $missing];
 }
