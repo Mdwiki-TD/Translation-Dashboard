cd6c7a05cadce8cbd6e1c1e871805b0b3f6be705
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-22 02:42:37 +0300

    Update new_sql_tables.php

--- src/backend/api_or_sql/new_sql_tables.php
+++ src/backend/api_or_sql/new_sql_tables.php
@@ -6,7 +6,6 @@ namespace SQLorAPI\Funcs;
 
 Usage:
 
-use function SQLorAPI\Funcs\exists_by_qids_query_and_category;
 use function SQLorAPI\Funcs\exists_by_qids_query;
 use function SQLorAPI\Funcs\get_missing_exists_statics;
 use function SQLorAPI\Funcs\missing_by_lang_and_category;
@@ -16,38 +15,6 @@ use function SQLorAPI\Funcs\exists_by_lang_and_category;
 
 use function SQLorAPI\Get\super_function;
 // use function SQLorAPI\Get\isvalid;
-
-
-function exists_by_qids_query_and_category($lang, $category)
-{
-    // ---
-    // http://localhost:9001/api.php?get=exists_by_qids&lang=ar&category=RTT&target=not_empty
-    static $data2 = [];
-    // ---
-    if (!empty($data2[$lang . $category] ?? [])) {
-        return $data2[$lang . $category];
-    }
-    // ---
-    $api_params = ['get' => 'exists_by_qids', 'lang' => $lang, 'category' => $category, 'target' => 'not_empty'];
-    // ---
-    $query = <<<SQL
-        SELECT a.qid, a.title, a.category, t.code, t.target
-            FROM all_qids_titles a
-            JOIN all_qids_exists t
-            ON t.qid = a.qid
-        WHERE t.code = ?
-        AND a.category = ?
-        AND (t.target != '' AND t.target IS NOT NULL)
-    SQL;
-    // ---
-    $params = [$lang, $category];
-    // ---
-    $u_data = super_function($api_params, $params, $query, "all_qids_titles");
-    // ---
-    $data2[$lang . $category] = $u_data;
-    // ---
-    return $u_data;
-}
 
 function exists_by_qids_query($lang)
 {
@@ -63,12 +30,15 @@ function exists_by_qids_query($lang)
     $api_params = ['get' => 'exists_by_qids', 'lang' => $lang, 'target' => 'not_empty'];
     // ---
     $query = <<<SQL
-        SELECT a.qid, a.title, a.category, t.code, t.target
-            FROM all_qids_titles a
-            JOIN all_qids_exists t
-            ON t.qid = a.qid
+        SELECT
+            a.qid AS qid,
+            a.title AS title,
+            a.category AS category,
+            t.code AS code,
+            t.target AS target
+        FROM all_qids_titles a
+            JOIN all_qids_exists t ON t.qid = a.qid
         WHERE t.code = ?
-
         AND (t.target != '' AND t.target IS NOT NULL)
     SQL;
     // ---
@@ -89,7 +59,6 @@ function get_missing_exists_statics($category)
         $category = "RTT";
     }
     // ---
-    // http://localhost:9001/api.php?get=exists_by_qids&lang=ar&category=RTT&target=not_empty
     static $data2 = [];
     // ---
     if (!empty($data2[$category] ?? [])) {
