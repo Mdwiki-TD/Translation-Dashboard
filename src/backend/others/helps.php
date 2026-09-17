be46840428a0fe10ca1150be2bba8321b9364cef
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-17 08:08:55 +0300

    Update helps.php

--- src/backend/others/helps.php
+++ src/backend/others/helps.php
@@ -65,7 +65,7 @@ function make_translate_urls(
     $in_progress_translation_button,
     $_user_,
     $full_tr_user,
-    $_user_no_as_global_username,
+    $login_user_is_not_the_translator,
     $endpoint
 ) {
     //---
@@ -123,7 +123,7 @@ function make_translate_urls(
     }
     //---
     if ($inprocess) {
-        if ($in_progress_translation_button != 1 && $_user_no_as_global_username) {
+        if ($in_progress_translation_button != 1 && $login_user_is_not_the_translator) {
             $tab = '';
             $translate_url = $mdwiki_url;
             $full_translate_url = $mdwiki_url;
