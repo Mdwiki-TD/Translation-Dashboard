1399a43e11e510b0be6ce7b45d42c4d8765b9147
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-18 02:37:24 +0300

    Switch main index to 2027 results loader
    Update src/index.php to use the new Results\GetResults27\ResultsLoader instead of the deprecated results_loader_2026 helper. This aligns the main page with the current results pipeline and keeps the loader invocation consistent with the object-based API.

--- src/index.php
+++ src/index.php
@@ -22,8 +22,7 @@ use function Loaders\LoadRequest\load_request;
 use function SQLorAPI\GetDataTab\get_td_or_sql_categories;
 use function SQLorAPI\GetDataTab\get_td_or_sql_settings;
 use function SQLorAPI\GetDataTab\get_td_or_sql_langs;
-// use Results\GetResults2026\ResultsLoader;
-use function Results\GetResults2026\results_loader_2026;
+use Results\GetResults27\ResultsLoader;
 
 
 function make_drop($uxutable, $code)
@@ -282,9 +281,8 @@ if ($camp && $code) {
         "test" => $test
     ];
     // ---
-    // $loader = new ResultsLoader();
-    // echo $loader->load($data);
-    echo results_loader_2026($data);
+    $loader = new ResultsLoader();
+    echo $loader->load($data);
 }
 
 echo "</div><br>";
