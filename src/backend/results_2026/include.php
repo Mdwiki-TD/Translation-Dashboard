254ece6a62dfad4604d3d67ad535f002cce25367
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-18 01:58:21 +0300

    Update include.php

--- src/backend/results_2026/include.php
+++ src/backend/results_2026/include.php
@@ -1,8 +1,9 @@
 <?php
 
+include_once __DIR__ . "/results_table.php";
+include_once __DIR__ . "/results_table_exists.php";
+include_once __DIR__ . "/results_table_inprocess.php";
 
-include_once __DIR__ . '/ResultsLoader.php';
-include_once __DIR__ . '/index.php';
-include_once __DIR__ . '/results_table.php';
-include_once __DIR__ . '/results_table_exists.php';
-include_once __DIR__ . '/results_table_inprocess.php';
+// Main loader
+include_once __DIR__ . "/ResultsLoader.php";
+include_once __DIR__ . "/index.php";
