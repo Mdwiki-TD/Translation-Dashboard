5f745e5080885ef9a2690df611938b0b69e5d606
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-18 01:57:20 +0300

    .

--- src/backend/results_2026/include.php
+++ src/backend/results_2026/include.php
@@ -1,7 +1,7 @@
 <?php
 
 
-include_once __DIR__ . '/get_results_2026.php';
+include_once __DIR__ . '/ResultsLoader.php';
 include_once __DIR__ . '/index.php';
 include_once __DIR__ . '/results_table.php';
 include_once __DIR__ . '/results_table_exists.php';
